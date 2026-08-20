f3b74077aba1e5154055dad2a2c989ee17159fd9
LoKal-CODE <devin.grush2007@gmail.com>
2026-08-20 12:25:06 -0600

    Commit machine over here

--- Lecture/test.cc
+++ Lecture/test.cc
@@ -5,6 +5,15 @@ int main()
 
     std::cout << "Hello\n";
 
+    bool a = false;
+
+    if (a == true) {
+        std::cout << "a is true\n";
+    } else {
+        std::cout << "a is false\n";
+    }
+
+
     return 0;
 
 }
