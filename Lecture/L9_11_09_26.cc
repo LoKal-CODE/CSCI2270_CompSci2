3db860d58748aa38bbc3928eef244b3aaac1520b
LoKal-CODE <devin.grush2007@gmail.com>
2026-09-11 15:18:19 -0600

    Filled out L9

--- Lecture/L9_11_09_26.cc
+++ Lecture/L9_11_09_26.cc
@@ -5,5 +5,32 @@ using namespace std;
 int main( int argc, char* argv[] )
 {
 
+    int* a = new int[5]; // creates an integer array of 5 elements
+
+    int* b = new int(5); // is the same as int* b; *b = 5;
+
+    delete a;
+    delete b;
+    a = NULL;
+    b = NULL;
+
+    // array doubling
+
+    int n = 3;
+    int* c = new int[n];
+    int* temp = new int[2 * n];
+    for (int i = 0; i < n; i++)
+    {
+        temp[i] = a[i];
+    }
+    delete[] c; // delete BEFORE c = temp to not cause memory leak by pointing c to a different array before deallocating it (if you dealloc then, then you would be deallocing the new, bigger array)
+    c = temp; // address of c is now that of the new, bigger array in heap memory
+    temp = NULL; // good practice, remove the value stored in temp by setting it to NULL or nullptr
+    n = 2*n;
+    
+    delete[] temp;
+    c = NULL;
+    temp = NULL; 
+
     return 0;
 }
