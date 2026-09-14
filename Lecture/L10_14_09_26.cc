6f5776164b7abee7e42dfe6c57e4aa3072d23d6c
LoKal-CODE <devin.grush2007@gmail.com>
2026-09-14 15:19:14 -0600

    Filled out L10

--- Lecture/L10_14_09_26.cc
+++ Lecture/L10_14_09_26.cc
@@ -1,15 +1,46 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+// pointers to structs
+struct Student
+{
+    string name;
+    int age;
+};
 
+class Robot
+{
+    private:
+        string name;
+        int batLevel;
 
+    public:
+        void setName(string n)
+        {
+            name = n;
+        }
+};
 
 
 int main(void)
 {
     
+    Student s0;
+    Student* sptr = NULL;
 
+    s0.age = 344;
+    s0.name = "Pat";
+
+    sptr = &s0;
+
+    sptr -> name = "Bob";
+    sptr -> age = 34;
+
+    Robot* myRobot = new Robot();
+
+    myRobot -> setName("Randy");
 
     return 0;
 }
